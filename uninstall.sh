5fe80ba1eedf0a80433f6246f728e26d6979b202
Kevin Alles <git-message@kevin-alles.de>
2025-09-21 23:59:30 +0200

    Add uninstall script for complete removal of quiz application

---
+++ uninstall.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+set -e
+
+# Set log file location
+LOG_FILE=/tmp/uninstall.log
+
+# Truncate log file at start
+: > "$LOG_FILE"
+
+# Redirect all output (stdout and stderr) to log file and console
+exec > >(tee -a $LOG_FILE) 2>&1
+
+# Log start time
+echo "Deinstallation gestartet um $(date +"%d.%m.%Y %H:%M:%S")"
+
+sudo userdel quiz
+sudo apt purge -y apache2 libapache2-mod-php php git webhook sudo
+rm -r /etc/apache2/
+rm -r /opt/quiz/
+rm -r /etc/systemd/system/quiz-backend.service
+rm -r /etc/webhook/
+rm -r /etc/systemd/system/webhook.service
+rm -r /etc/sudoers.d/
+
+echo "Deinstallation abgeschlossen um $(date +"%d.%m.%Y %H:%M:%S")"
