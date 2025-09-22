832dd17154037bf3f3d4998ff7e08c2c6794fe54
Kevin Alles <git-message@kevin-alles.de>
2025-09-22 19:47:58 +0200

    Refactor uninstall.sh to disable Apache modules before package removal for improved cleanup process

--- uninstall.sh
+++ uninstall.sh
@@ -15,13 +15,16 @@ exec > >(tee -a $LOG_FILE) 2>&1
 echo "Deinstallation gestartet um $(date +"%d.%m.%Y %H:%M:%S")"
 
 # Remove user, group, installed packages, application files, services and configurations
-apt-get remove --purge -y git apache2 libapache2-mod-php php webhook sudo
+sudo a2dismod mpm_worker
+sudo a2dismod mpm_prework
+sudo a2dismod mpm_event
+sudo apt-get remove --purge -y git apache2 libapache2-mod-php php webhook sudo
+apt autoremove -y
 userdel quiz
 rm -r /etc/apache2/
 rm -r /opt/quiz/
 rm -r /etc/webhook/
 rm -r /etc/sudoers.d/
-apt autoremove -y
 
 # Stop and disable services
 systemctl stop quiz-backend.service || true
